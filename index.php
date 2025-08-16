b49472e966a3cde33683e1bb312f8a6963aa9688
MRxwr <nasserhatab@gmail.com>
2025-08-16 14:05:39 +0300

    save

--- index.php
+++ index.php
@@ -2,20 +2,7 @@
 include("admin/includes/config.php");
 include("admin/includes/functions.php");
 include("admin/includes/translate.php");
-/*
-if( isset($_GET["result"]) && !empty($_GET["result"]) ){
-	if( isset($_GET["requested_order_id"]) && !empty($_GET["requested_order_id"]) && $order = selectDBNew("orders2",[$_GET["requested_order_id"]],"`gatewayId` LIKE ?","") ){
-		if( $order[0]["status"] == 0 ){
-			if( $_GET["result"] == "success" ){
-				updateDB("orders2",array("status"=>1),"`id` = '{$order[0]["id"]}'");
-			}else{
-				updateDB("orders2",array("status"=>5),"`id` = '{$order[0]["id"]}'");
-			}
-			whatsappUltraMsg($order[0]["id"]);
-		}
-	}
-}
-*/
+
 if( isset($_GET["v"]) && searchFile("views","blade{$_GET["v"]}.php") ){
 	require_once("views/".searchFile("views","blade{$_GET["v"]}.php"));
 }else{
